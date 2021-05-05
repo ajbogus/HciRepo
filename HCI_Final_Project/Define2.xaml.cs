5f8480f5adaa21f5f76cc25f70ca3f0dc00bf9ae
ajbogus <ajbogus@outlook.com>
2021-05-05 15:48:11 -0600

    Added the working dictionary API!!!. WIP on the audio

--- HCI_Final_Project/Define2.xaml.cs
+++ HCI_Final_Project/Define2.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Net.Http;
 
 namespace HCI_Final_Project
 {
@@ -23,12 +24,70 @@ namespace HCI_Final_Project
             InitializeComponent();
         }
 
-        public Define2(String wordtoDefine)
+        public  Define2(String wordtoDefine)
         {
             InitializeComponent();
             word = wordtoDefine;
             wordLabel.Content = word;
+            defineWord(word);
             
         }
+
+
+        public async void defineWord(string word)
+        {
+            Console.WriteLine("Getting to define word Yay!!!");
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    //var stringContent = new StringContent("", System.Text.Encoding.UTF8, "application/json");
+                    var response = await client.GetAsync("https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key=8e6c9461-437f-4dea-93f7-9b0cd218d5a0");
+                    var result = await response.Content.ReadAsStringAsync();
+                    
+                    parseDefinition(result);
+                    defLabel.Content = result;
+                }
+                catch (Exception ex)
+                {
+                    //TODO: popup an error message
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.ResetColor();
+                }
+            }
+        }
+
+
+        //This methhod will parse the API response into 3 objects
+        //  1. Definition
+        //  2. Syllable layout
+        public void parseDefinition(string word_response)
+        {
+
+            //2.
+            int indexOfSyllable = word_response.IndexOf("\"hw\":");
+            indexOfSyllable = indexOfSyllable + 5;
+            int indexOfSyllableEnd = word_response.IndexOf(',', indexOfSyllable);
+            int syllableLength = indexOfSyllableEnd - indexOfSyllable;
+            string syllable = word_response.Substring(indexOfSyllable, syllableLength);
+            syllableLabel.Content = syllable;
+            //syllableLabel.FontFamily = System.Windows.Media.FontFamily("Arial");
+            syllableLabel.FontSize = 24;
+
+
+            //3.
+            int indexOfAudio = word_response.IndexOf("\"audio\":");
+            indexOfAudio = indexOfAudio + 9;
+            int indexOfAudioEnd = word_response.IndexOf(",", indexOfAudio);
+            indexOfAudioEnd = indexOfAudioEnd-1;
+            int audioLength = indexOfAudioEnd - indexOfAudio;
+            string audiofile = word_response.Substring(indexOfAudio, audioLength);
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(audiofile + ".wav");
+            player.Play();
+
+
+        }
+      
     }
 }
